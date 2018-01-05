aca6dd75412becd26309ea0d5c0f3a3f31737d66
Jacob Brown <kardeiz@gmail.com>
2018-01-05 07:05:09 -0600

    add refund create

--- src/resources/refund.rs
+++ src/resources/refund.rs
@@ -1,3 +1,5 @@
+use client::Client;
+use error::{Error, ErrorCode};
 use params::{Metadata, Timestamp};
 use resources::Currency;
 
@@ -19,4 +21,24 @@ pub struct Refund {
     pub reason: Option<String>, // (duplicate, fraudulent, requested_by_customer)
     pub receipt_number: Option<String>,
     pub status: String, // (succeeded, pending, failed, cancelled)
+}
+
+#[derive(Default, Serialize)]
+pub struct RefundParams<'a> {
+    pub charge: &'a str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub amount: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub metadata: Option<Metadata>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub reason: Option<&'a str>,
+}
+
+impl Refund {
+    /// Creates a new charge.
+    ///
+    /// For more details see https://stripe.com/docs/api#create_refund.
+    pub fn create(client: &Client, params: RefundParams) -> Result<Refund, Error> {
+        client.post("/refunds", params)
+    }
 }
